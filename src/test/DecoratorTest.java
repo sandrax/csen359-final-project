65e2430fbf329e70c6ab021c37d06eb93cc2d847
divshett <101359249+divshett@users.noreply.github.com>
2025-06-07 00:52:21 -0700

    update DecoratorTest.java

--- src/test/DecoratorTest.java
+++ src/test/DecoratorTest.java
@@ -11,19 +11,7 @@ import ingredients.Powdered;
 
 public class DecoratorTest {
     public static void main(String[] args) {
-        System.out.println("\n--- Gathering Ingredients ---\n");
-
-        Aconite a1 = new Aconite("root");
-        Aconite a2 = a1.geminio();
-        System.out.println("new aconite?: " + (a1 != a2));
-
-        Mandrake m1 = new Mandrake();
-        Mandrake m2 = m1.geminio();
-        System.out.println("new mandrake?: " + (m1 != m2));
-
-        OccamyEgg o1 = new OccamyEgg();
-        OccamyEgg o2 = o1.geminio();
-        System.out.println("new egg?: " + (o1 != o2));
+        System.out.println("\n--- Gathering Decorated Ingredients ---\n");
 
         // test decorator (combined with prototype)
         BasicIngredient i = new Powdered(new Moonstone());
